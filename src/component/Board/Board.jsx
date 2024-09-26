39facb063eff0bc54e13d35a496b5c52435c659b
Sharif-Miah <farhansharif1410@gmail.com>
2024-09-26 21:27:08 +0600

    tik tok toe game board complete

--- src/component/Board/Board.jsx
+++ src/component/Board/Board.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import Button from "../Button/Button";
+// import calculateWinner from "../CalculateWinner/CalculateWinner";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  let status;
+
   const handleBoard = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
 
     if (xIsNext) {
@@ -18,8 +25,15 @@ export default function Board() {
     setXIsNext(!xIsNext);
   };
 
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div>
+      <h1>{status}</h1>
       <div className="flex">
         <Button value={squares[0]} handleSqure={() => handleBoard(0)} />
         <Button value={squares[1]} handleSqure={() => handleBoard(1)} />
@@ -37,4 +51,24 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
